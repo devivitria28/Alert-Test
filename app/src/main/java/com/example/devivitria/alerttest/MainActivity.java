0311bef1208a171533b96ed79f542bc9b5715526
devivitria28 <vitriadevi28@gmail.com>
2018-01-13 02:50:21 +0700

    first commit

---
+++ app/src/main/java/com/example/devivitria/alerttest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.devivitria.alerttest;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Set;
+
+import static android.R.attr.button;
+import static com.example.devivitria.alerttest.R.attr.dialogPreferredPadding;
+import static com.example.devivitria.alerttest.R.attr.title;
+
+
+
